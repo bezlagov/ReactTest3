14d65e018135fc881ef1d8b993a05e428cf25cd6
Ivan <oppositelockgames@users.noreply.github.com>
2022-02-16 16:03:16 +0200

    set events

---
+++ src/components/Counter/counter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../../App.css';
+
+export default class Counter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+    increment(e){
+        this.setState({count: this.state.count+1});
+    }
+  
+    decrement(e){
+        this.setState({count: this.state.count-1});
+    }
+
+    render() {
+        return (
+            <div className='counterBox'>
+                <h1>{this.state.count}</h1>
+                <button onClick={(e)=>this.increment(e)}>+</button>
+                <button onClick={(e)=>this.decrement(e)}>-</button>
+            </div>);
+    }
+}
