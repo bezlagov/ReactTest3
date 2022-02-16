3b3618fbeef347897c124eb68cc0b83b44645e7a
Ivan <oppositelockgames@users.noreply.github.com>
2022-02-16 16:08:48 +0200

    add bg and checks

--- src/components/Counter/counter.js
+++ src/components/Counter/counter.js
@@ -8,20 +8,23 @@ export default class Counter extends React.Component {
             count: 0
         }
     }
-    increment(e){
-        this.setState({count: this.state.count+1});
+    increment(e) {
+        console.log();
+        this.setState({ count: this.state.count + 1 });
     }
-  
-    decrement(e){
-        this.setState({count: this.state.count-1});
+
+    decrement(e) {
+        if (this.state.count - 1 >= 0) {
+            this.setState({ count: this.state.count - 1 });
+        }
     }
 
     render() {
         return (
             <div className='counterBox'>
                 <h1>{this.state.count}</h1>
-                <button onClick={(e)=>this.increment(e)}>+</button>
-                <button onClick={(e)=>this.decrement(e)}>-</button>
+                <button onClick={(e) => this.increment(e)}>+</button>
+                <button onClick={(e) => this.decrement(e)}>-</button>
             </div>);
     }
 }
